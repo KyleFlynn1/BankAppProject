53b5153b394f4f2abe209cec2b0131c504c2b283
KyleFlynn1 <s00251601@atu.ie>
2025-03-27 00:34:29 +0000

    Added Create Account Feature to Login Page

--- BankApp/LoginPage.xaml.cs
+++ BankApp/LoginPage.xaml.cs
@@ -23,32 +23,107 @@ namespace BankApp
     public partial class LoginPage : Page
     {
         public static int UserID;
+        public bool CreatingAccount = false;
         UserData db = new UserData();
 
         public LoginPage()
         {
             InitializeComponent();
+            ShowLoginUI();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             MainWindow mainWindow = (MainWindow)Application.Current.MainWindow;
 
-            var loginQuery = (from p in db.Users
-                             where p.Password == passwordInput.Text && p.Username == usernameInput.Text
-                             select p.UserID).FirstOrDefault();
+            if (CreatingAccount == false)
+            {
+                var loginQuery = (from p in db.Users
+                                  where p.Password == passwordInput.Text && p.Username == usernameInput.Text
+                                  select p.UserID).FirstOrDefault();
 
-            if (loginQuery != 0)
-            {
-                txtError.Text = "";
-                LoginPage.UserID = loginQuery;
-                mainWindow.FullFrame.Content = null;
-                mainWindow.MainFrame.Navigate(new Uri("DashboardPage.xaml", UriKind.Relative));
+                if (loginQuery != 0)
+                {
+                    txtError.Text = "";
+                    LoginPage.UserID = loginQuery;
+                    mainWindow.FullFrame.Content = null;
+                    mainWindow.MainFrame.Navigate(new Uri("DashboardPage.xaml", UriKind.Relative));
+                }
+                else
+                {
+                    txtError.Text = "Incorrect Details Entered!";
+                }
             }
-            else
+            else if( CreatingAccount == true)
             {
-                txtError.Text = "Incorrect Details Entered!";
+                if(accountHolderInput.Text != "" && usernameInput.Text != "" && passwordInput.Text != "")
+                {
+                    //Make New Classes with information to add to database and be used
+                    UserAccount a1 = new UserAccount() { Username = usernameInput.Text, Password = passwordInput.Text };
+                    BankAccount b1 = new BankAccount(accountHolderInput.Text) { UserAccount = a1 };
+                    Card c1 = new Card("Debit", DateTime.Now) { BankAccount = b1 };
+                    b1.Cards.Add(c1);
+                    b1.CardCount = 1;
+                    a1.BankAccounts.Add(b1);
+
+                    //Add Data to DataBase and Save
+                    db.Users.Add(a1);
+                    db.BankAccounts.Add(b1);
+                    db.Cards.Add(c1);
+                    db.SaveChanges();
+
+                    var loginQuery = (from p in db.Users
+                                      where p.Password == passwordInput.Text && p.Username == usernameInput.Text
+                                      select p.UserID).FirstOrDefault();
+
+                    if (loginQuery != 0)
+                    {
+                        txtError.Text = "";
+                        CreatingAccount = false;
+                        LoginPage.UserID = loginQuery;
+                        mainWindow.FullFrame.Content = null;
+                        mainWindow.MainFrame.Navigate(new Uri("DashboardPage.xaml", UriKind.Relative));
+                    }
+                }
+                else
+                {
+                    txtError.Text = "Information Not Complete";
+                }
             }
+        }
+
+        private void createAccountBTN_Click(object sender, RoutedEventArgs e)
+        {
+            ShowCreateAccountUI();
+        }
+
+        private void loginAccountBTN_Click(object sender, RoutedEventArgs e)
+        {
+            ShowLoginUI();
+        }
+
+        public void ShowLoginUI()
+        {
+            txtAccountHolder.Visibility = Visibility.Hidden;
+            accountHolderInput.Visibility = Visibility.Hidden;
+            txtUserName.Text = "Username";
+            txtPassword.Text = "Password";
+            submitBTN.Content = "Login";
+            createAccountBTN.Visibility = Visibility.Visible;
+            loginAccountBTN.Visibility = Visibility.Hidden;
+            CreatingAccount = false;
+        }
+
+        public void ShowCreateAccountUI()
+        {
+            txtAccountHolder.Visibility = Visibility.Visible;
+            accountHolderInput.Visibility = Visibility.Visible;
+            txtUserName.Text = "Create Username";
+            txtPassword.Text = "Create Password";
+            submitBTN.Content = "Create Account";
+            loginAccountBTN.Visibility= Visibility.Visible;
+            createAccountBTN.Visibility = Visibility.Hidden;
+            CreatingAccount = true;
         }
     }
 }
